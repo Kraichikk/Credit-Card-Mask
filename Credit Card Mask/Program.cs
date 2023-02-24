5e31b42589c1f06ee0647cb2fc229257ea782f4b
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 17:14:00 +0200

    Add project files.

---
+++ Credit Card Mask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Credit_Card_Mask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maskify("1234567891011121314"));
+        }
+        public static string Maskify(string cc)
+        {
+            string result = "";
+
+            for (int i = 0; i < cc.Length; i++)
+            {
+                if (i < cc.Length - 4)
+                {
+                    result += '#';
+                }
+                else
+                {
+                    result += cc[i];
+                }
+            }
+            return result;
+        }
+    }
+}
